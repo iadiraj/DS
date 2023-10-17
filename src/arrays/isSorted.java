82cb79d16d4c19b8188eca92d0808bb01310594a
ADITYA RAJ <developer.rajaditya@gmail.com>
2023-10-18 00:04:06 +0530

    added STRIVER Find Second-Largest Element in Array | Remove duplicates from Sorted Array | Arrays Intro Video

---
+++ src/arrays/isSorted.java
@@ -0,0 +1,30 @@
+package arrays;
+//  Find Second Largest Element in Array | Remove duplicates from Sorted Array | Arrays Intro Video
+public class isSorted {
+    public static int isSorted(int n, int []nums) {
+        for(int i = 1; i < n; i++){
+            if(nums[i-1] > nums[i]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
+
+
+//  Problem Statement
+//  You have been given an array ‘a’ of ‘n’ non-negative integers.You have to check whether the given array is sorted in the non-decreasing order or not.
+//  Your task is to return 1 if the given array is sorted. Else, return 0.
+
+//  Example :
+//  Input: ‘n’ = 5, ‘a’ = [1, 2, 3, 4, 5]
+//  Output: 1
+//  The given array is sorted in non-decreasing order; hence the answer will be 1.
+
+//  Expected Time Complexity:
+//  O(n), Where ‘n’ is the size of an input array ‘a’.
+
+//  Constraints:
+//  1 ≤ 'n' ≤ 10^5
+//  0 ≤ 'a'[i] ≤ 10^9
+//  Time limit: 1 sec
