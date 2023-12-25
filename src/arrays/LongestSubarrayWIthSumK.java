f59a2c87bb0097a3579674eafc784ecbd9cf6666
ADITYA RAJ <developer.rajaditya@gmail.com>
2023-12-25 23:32:04 +0530

    added STRIVER Longest Subarray with sum K | Brute - Better - Optimal | Generate Subarrays

--- src/arrays/LongestSubarrayWIthSumK.java
+++ src/arrays/LongestSubarrayWIthSumK.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LongestSubarrayWIthSumK {
+//    Optimal Approach ( For postive numbers, zeros and negative numbers)
     public static int getLongestSubarray(int []a, int k) {
         Map<Long, Integer> map = new HashMap<>();
         Long sum = 0L;
@@ -24,4 +25,25 @@ public class LongestSubarrayWIthSumK {
         }
         return maxLen;
     }
+//    Optimal Approach ( For postive numbers, zeros)
+//    public static int getLongestSubarray(int[] a, int k) {
+//        int maxLen = 0;
+//        long sum = a[0];
+//        int r = 0, l = 0;
+//        int n = a.length;
+//        while (r < n) {
+//            while (l < r && sum > k) {
+//                sum -= a[l];
+//                l++;
+//            }
+//            if (sum == k) {
+//                maxLen = Math.max(maxLen, r - l + 1);
+//            }
+//            r++;
+//            if (r < n) {
+//                sum += a[r];
+//            }
+//        }
+//        return maxLen;
+//    }
 }
